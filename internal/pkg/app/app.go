bc5dc14977417c177e3840322a224b6a7195a035
balsamif <balsamif@student.21-school.ru>
2023-09-05 19:09:01 +0300

    First commit

---
+++ internal/pkg/app/app.go
@@ -0,0 +1,41 @@
+package app
+
+import (
+	"example/internal/app/endpoint"
+	"example/internal/app/mw"
+	"example/internal/app/service"
+	"fmt"
+	"log"
+
+	"github.com/labstack/echo/v4"
+)
+
+type App struct {
+	e    *endpoint.Endpoint
+	s    *service.Service
+	echo *echo.Echo
+}
+
+func New() (*App, error) {
+	a := &App{}
+
+	a.s = service.New()
+	a.e = endpoint.New(a.s)
+
+	a.echo = echo.New()
+
+	a.echo.Use(mw.RoleCheck)
+	a.echo.GET("/status", a.e.Status)
+
+	return a, nil
+}
+
+func (a *App) Run() error {
+	fmt.Println("server is running...")
+	err := a.echo.Start(":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nil
+}
